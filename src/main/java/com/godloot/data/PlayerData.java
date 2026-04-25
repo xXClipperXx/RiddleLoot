26c720d6bfa3519c09bc4bef3877c52afcd4395d
xXClipperXx <justclipperxx@gmail.com>
2026-04-24 22:47:20 -0300

    Created PlayerData class in PlayerData.java

---
+++ src/main/java/com/godloot/data/PlayerData.java
@@ -0,0 +1,25 @@
+public class PlayerData {
+    private String playerName;
+    private int playerScore;
+
+    public PlayerData(String playerName, int playerScore) {
+        this.playerName = playerName;
+        this.playerScore = playerScore;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public void setPlayerScore(int playerScore) {
+        this.playerScore = playerScore;
+    }
+}
