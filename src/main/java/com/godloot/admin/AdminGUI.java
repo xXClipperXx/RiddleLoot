b044ad6244a0d03711280fb2319b114215f7a823
xXClipperXx <justclipperxx@gmail.com>
2026-04-24 22:47:47 -0300

    Create AdminGUI class

---
+++ src/main/java/com/godloot/admin/AdminGUI.java
@@ -0,0 +1,12 @@
+public class AdminGUI {
+
+    // Constructor
+    public AdminGUI() {
+        // Initialize the GUI components here
+    }
+
+    // Methods to handle GUI actions
+    public void show() {
+        // Code to display the GUI
+    }
+}
